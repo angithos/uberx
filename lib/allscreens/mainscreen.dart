9b43d6df6e751757059bfb3bcda4d619063cd749
angith <angithkrishna07@gmail.com>
2024-01-20 07:36:46 +0300

    main screen design changed

--- lib/allscreens/mainscreen.dart
+++ lib/allscreens/mainscreen.dart
@@ -1,25 +1,25 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
+import 'package:uber_x/all%20widgets/Divider.dart';
 
 class Mainscreen extends StatefulWidget {
   const Mainscreen({super.key});
-  static const String idScreen ="mainscreen";
+  static const String idScreen = "mainscreen";
   @override
   State<Mainscreen> createState() => _MainscreenState();
 }
 
 class _MainscreenState extends State<Mainscreen> {
-
-
   final Completer<GoogleMapController> _controllerGoogleMap =
       Completer<GoogleMapController>();
 
-      late GoogleMapController newGoogleMapController;
-      
-    static const CameraPosition _kGooglePlex = CameraPosition(
+  late GoogleMapController newGoogleMapController;
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(26.188457391704546, 50.50562990481436),
     zoom: 17.4746,
   );
@@ -27,25 +27,119 @@ class _MainscreenState extends State<Mainscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Uber X"),
-        backgroundColor:Colors.amberAccent.shade400,
-        
-      ),
-      body: Stack(
-        children: [
-    GoogleMap(
-      mapType: MapType.normal,
-      myLocationButtonEnabled: true,
-      initialCameraPosition: _kGooglePlex,
-      onMapCreated: (GoogleMapController controller){
-       _controllerGoogleMap.complete(controller);
-       newGoogleMapController=controller;
-      },
-    )
-      ],)
-        
-    ); 
-    
+        appBar: AppBar(
+          title: Text("Uber X"),
+          backgroundColor: Colors.amberAccent.shade400,
+        ),
+        body: Stack(
+          children: [
+            GoogleMap(
+              mapType: MapType.normal,
+              myLocationButtonEnabled: true,
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                _controllerGoogleMap.complete(controller);
+                newGoogleMapController = controller;
+              },
+            ),
+            Positioned(
+              left: 0.0,
+              right: 0.0,
+              bottom: 0.0,
+              child: Container(
+                height: 320.0,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15.0),
+                        topRight: Radius.circular(15.0)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 16.0,
+                        spreadRadius: 0.6,
+                        offset: Offset(0.7, 0.7),
+                      )
+                    ]),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 24.0, vertical: 18.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 6.0,
+                      ),
+                      Text("Hi there", style: TextStyle(fontSize: 12.0)),
+                      Text("Where do you want to go",
+                          style: TextStyle(
+                              fontSize: 20.0, fontFamily: "Brand-Bold")),
+                      SizedBox(
+                        height: 6.0,
+                      ),
+                      Container(
+                       
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(5.0),
+                            // ignore: prefer_const_literals_to_create_immutables
+                            boxShadow: [
+                              // ignore: prefer_const_constructors
+                              BoxShadow(
+                                color: Colors.black54,
+                                blurRadius: 6.0,
+                                spreadRadius: 0.6,
+                                offset: Offset(0.7, 0.7),
+                              ),
+                              
+                            ]),
+                            child: Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Row(children: [
+                                SizedBox(width: 2.0,),
+                                Icon(Icons.search,color: Colors.blueAccent,size: 25.0,),
+                              SizedBox(width: 10.0,),
+                              Text("Search Drop off",style: TextStyle(fontSize: 15.0),)
+                              ]),
+                            ),
+                      ),
+                      SizedBox(height: 24.0,),
+                      Row(children: [
+                        Icon(Icons.home,color: Colors.grey,),
+                        SizedBox(width: 12.0,),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                        Text("Add home"),
+                        SizedBox(height: 4.0,),
+                        Text("Your living home address",style: TextStyle(color: Colors.black54,fontSize: 12.0),)
+                          ],
+                        )
+                      ],
+                      ),
+                       SizedBox(height: 10.0),
+
+                       DividerWidget(),
+                       SizedBox(height: 16.0),
+                      Row(children: [
+                        Icon(Icons.work,color: Colors.grey,),
+                        SizedBox(width: 12.0,),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                        Text("Add work"),
+                        SizedBox(height: 4.0,),
+                        Text("Your office address",style: TextStyle(color: Colors.black54,fontSize: 12.0),)
+                          ],
+                        )
+                      ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
