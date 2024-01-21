52aaa0fbb2e286804a2c2a3be46c60242c027f33
angith <angithkrishna07@gmail.com>
2024-01-21 09:54:05 +0300

    current location function added

--- lib/allscreens/mainscreen.dart
+++ lib/allscreens/mainscreen.dart
@@ -3,6 +3,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:uber_x/all%20widgets/Divider.dart';
 import 'package:uber_x/allscreens/location_help.dart';
@@ -14,13 +15,61 @@ class Mainscreen extends StatefulWidget {
   State<Mainscreen> createState() => _MainscreenState();
 }
 
-class _MainscreenState extends State<Mainscreen> { 
-
+class _MainscreenState extends State<Mainscreen> {
   final Completer<GoogleMapController> _controllerGoogleMap =
       Completer<GoogleMapController>();
 
   late GoogleMapController newGoogleMapController;
-  final GlobalKey<ScaffoldState> scaffoldKey =new GlobalKey<ScaffoldState> ();
+  
+  final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+  late Position currentPosition;
+  var geolocator=Geolocator();
+  double paddingForMap=0;
+//old function
+void locatePosition()async{
+  
+ Position position =await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high,forceAndroidLocationManager:true);
+
+  currentPosition=position;
+  LatLng latlnPosition=LatLng(position.latitude, position.longitude);
+
+  CameraPosition cameraPosition=new CameraPosition(target: latlnPosition,zoom: 16.0);
+  newGoogleMapController.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  }
+  
+//new function
+
+Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    permission = await Geolocator.requestPermission();
+
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high ,forceAndroidLocationManager:true );
+    currentPosition=position;
+  LatLng latlnPosition=LatLng(position.latitude, position.longitude);
+
+  CameraPosition cameraPosition=new CameraPosition(target: latlnPosition,zoom: 12.0);
+  newGoogleMapController.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+
+    return await Geolocator.getCurrentPosition();
+
+  }
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(26.188457391704546, 50.50562990481436),
@@ -30,97 +79,120 @@ class _MainscreenState extends State<Mainscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: scaffoldKey,
+        key: scaffoldKey,
         appBar: AppBar(
-          title: Text("Uber X"),
+          title: Text("K-App"),
           backgroundColor: Colors.amberAccent.shade400,
         ),
-        drawer: 
-          // color: Colors.white,
-          // width: 55.0,
-          // height: 75.0,
-           Drawer(
-            child: ListView(
-              
-              children: [
-                //drawer header
-                UserAccountsDrawerHeader(
-                    
-                    accountName: Text("Profile name", style: TextStyle(fontSize: 16.0, fontFamily: "Brand-Bold" ,color: Colors.black)),
-                    accountEmail: Text("show profile",style: TextStyle(color: Colors.black54),),
-                    currentAccountPicture: CircleAvatar(
-                    backgroundImage: AssetImage("images/user_icon.png"),
-                    ),
-                    decoration: BoxDecoration(color: Colors.white),
+        drawer:
+            // color: Colors.white,
+            // width: 55.0,
+            // height: 75.0,
+            Drawer(
+          child: ListView(
+            children: [
+              //drawer header
+              UserAccountsDrawerHeader(
+                accountName: Text("Profile name",
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        fontFamily: "Brand-Bold",
+                        color: Colors.black)),
+                accountEmail: Text(
+                  "show profile",
+                  style: TextStyle(color: Colors.black54),
                 ),
-                // DividerWidget(),
-                SizedBox(height: 12.0,),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage("images/user_icon.png"),
+                ),
+                decoration: BoxDecoration(color: Colors.white),
+              ),
+              // DividerWidget(),
+              SizedBox(
+                height: 12.0,
+              ),
 
-                //drawer body controllers
-                ListTile(
-                  leading: Icon(Icons.history),
-                  title: Text("History",style: TextStyle(fontSize: 15.0),),
+              //drawer body controllers
+              ListTile(
+                leading: Icon(Icons.history),
+                title: Text(
+                  "History",
+                  style: TextStyle(fontSize: 15.0),
                 ),
-                ListTile(
-                  leading: Icon(Icons.person),
-                  title: Text("Visit profile",style: TextStyle(fontSize: 15.0),),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text(
+                  "Visit profile",
+                  style: TextStyle(fontSize: 15.0),
                 ),
-                ListTile(
-                  leading: Icon(Icons.info),
-                  title: Text("About",style: TextStyle(fontSize: 15.0),),
+              ),
+              ListTile(
+                leading: Icon(Icons.info),
+                title: Text(
+                  "About",
+                  style: TextStyle(fontSize: 15.0),
                 ),
-              ],
-            ),
+              ),
+                
+            ],
           ),
-        
+        ),
         body: Stack(
-          
           children: [
             Expanded(
               child: GoogleMap(
+                padding: EdgeInsets.only(bottom: paddingForMap),
                 mapType: MapType.normal,
                 myLocationButtonEnabled: true,
                 initialCameraPosition: _kGooglePlex,
+                myLocationEnabled: true,
+                zoomGesturesEnabled: true,
+                zoomControlsEnabled: true,
                 onMapCreated: (GoogleMapController controller) {
                   _controllerGoogleMap.complete(controller);
                   newGoogleMapController = controller;
-              //      LocationHelper.checkAndRequestLocationPermission(
-              //   newGoogleMapController,
-              // );
+                  //      LocationHelper.checkAndRequestLocationPermission(
+                  //   newGoogleMapController,
+                  // );
+                  setState(() {
+                    paddingForMap=300.0;
+                  });
+                  locatePosition();
+                  // determinePosition();
                 },
               ),
             ),
             //hamburgerbutton for drawer
 
             Positioned(
-              
-              top: 45.0 ,
+              top: 45.0,
               left: 22.0,
-
               child: GestureDetector(
-                onTap: (){
+                onTap: () {
                   print("button pressed");
                   scaffoldKey.currentState?.openDrawer();
                 },
                 child: Container(
-                decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(22.0),
-                boxShadow: [
-                  BoxShadow(
-                    color:Colors.black,
-                    blurRadius: 6.0,
-                    spreadRadius: 0.5,
-                    offset: Offset(0.7, 0.7)
-                  )
-                ]
-                            ),
-                            child: CircleAvatar(
-                backgroundColor: Colors.white,
-                child: Icon(Icons.menu,color: Colors.black,),
-                radius: 20.0,
-                            ),
-                            ),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(22.0),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black,
+                            blurRadius: 6.0,
+                            spreadRadius: 0.5,
+                            offset: Offset(0.7, 0.7))
+                      ]),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Icon(
+                      Icons.menu,
+                      color: Colors.black,
+                    ),
+                    radius: 20.0,
+                  ),
+                ),
               ),
             ),
             Positioned(
@@ -159,7 +231,6 @@ class _MainscreenState extends State<Mainscreen> {
                         height: 6.0,
                       ),
                       Container(
-                       
                         decoration: BoxDecoration(
                             color: Colors.white,
                             borderRadius: BorderRadius.circular(5.0),
@@ -172,47 +243,83 @@ class _MainscreenState extends State<Mainscreen> {
                                 spreadRadius: 0.6,
                                 offset: Offset(0.7, 0.7),
                               ),
-                              
                             ]),
-                            child: Padding(
-                              padding: const EdgeInsets.all(12.0),
-                              child: Row(children: [
-                                SizedBox(width: 2.0,),
-                                Icon(Icons.search,color: Colors.blueAccent,size: 25.0,),
-                              SizedBox(width: 10.0,),
-                              Text("Search Drop off",style: TextStyle(fontSize: 15.0),)
-                              ]),
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Row(children: [
+                            SizedBox(
+                              width: 2.0,
                             ),
+                            Icon(
+                              Icons.search,
+                              color: Colors.blueAccent,
+                              size: 25.0,
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            Text(
+                              "Search Drop off",
+                              style: TextStyle(fontSize: 15.0),
+                            )
+                          ]),
+                        ),
                       ),
-                      SizedBox(height: 24.0,),
-                      Row(children: [
-                        Icon(Icons.home,color: Colors.grey,),
-                        SizedBox(width: 12.0,),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                        Text("Add home"),
-                        SizedBox(height: 4.0,),
-                        Text("Your living home address",style: TextStyle(color: Colors.black54,fontSize: 12.0),)
-                          ],
-                        )
-                      ],
+                      SizedBox(
+                        height: 24.0,
                       ),
-                       SizedBox(height: 10.0),
-                       DividerWidget(),
-                       SizedBox(height: 16.0),
-                      Row(children: [
-                        Icon(Icons.work,color: Colors.grey,),
-                        SizedBox(width: 12.0,),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                        Text("Add work"),
-                        SizedBox(height: 4.0,),
-                        Text("Your office address",style: TextStyle(color: Colors.black54,fontSize: 12.0),)
-                          ],
-                        )
-                      ],
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.home,
+                            color: Colors.grey,
+                          ),
+                          SizedBox(
+                            width: 12.0,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Add home"),
+                              SizedBox(
+                                height: 4.0,
+                              ),
+                              Text(
+                                "Your living home address",
+                                style: TextStyle(
+                                    color: Colors.black54, fontSize: 12.0),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 10.0),
+                      DividerWidget(),
+                      SizedBox(height: 16.0),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.work,
+                            color: Colors.grey,
+                          ),
+                          SizedBox(
+                            width: 12.0,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Add work"),
+                              SizedBox(
+                                height: 4.0,
+                              ),
+                              Text(
+                                "Your office address",
+                                style: TextStyle(
+                                    color: Colors.black54, fontSize: 12.0),
+                              )
+                            ],
+                          )
+                        ],
                       ),
                     ],
                   ),
